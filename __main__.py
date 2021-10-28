a3dd3414333c41289e8658090b231f4a341dc776
josunx <josue.n.2000@outlook.com>
2021-10-28 18:20:17 -0500

    Cambio 3

--- __main__.py
+++ __main__.py
@@ -10,13 +10,13 @@ camera = cv2.VideoCapture(0)
 hsv_min= (29, 86, 6)
 hsv_max= (64, 255, 255)
 
-def find_object(frame, mascara, color):
-	cnts, hierarchy = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	c = max(cnts, key= cv2.contourArea)
-
-	x,y,w,h = cv2.boundingRect(c)
-	cv2.rectangle(frame, (x,y),(x+w,y+h), color, 2)
-	return (round(x+w/2), round(y+h/2))
+#def find_object(frame, mascara, color):
+#	cnts, hierarchy = cv2.findContours(mascara, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#	c = max(cnts, key= cv2.contourArea)
+#
+#	x,y,w,h = cv2.boundingRect(c)
+#	cv2.rectangle(frame, (x,y),(x+w,y+h), color, 2)
+#	return (round(x+w/2), round(y+h/2))
 
 while True:
 	(grabbed, frame) = camera.read()
@@ -26,10 +26,13 @@ while True:
 
 	hsv= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	mascara = cv2.inRange(hsv, hsv_min, hsv_max)
-	pg= find_object(frame, mascara, (255,0,0))
-
+	#pg= find_object(frame, mascara, (255,0,0))
+	kernel = np.ones((3,3), np.uint8)
+	erosion = cv2.erode(mascara, kernel, iterations=4)
+	dilation = cv2.dilate(mascara, kernel, iterations=1)
 	cv2.imshow('frame', frame)
-	#cv2.imshow('camara' ,mascara)
+	cv2.imshow('mask', mascara)
+	cv2.imshow('dilate' ,dilation)
 
 	k = cv2.waitKey(1) & 0xFF
 	if k==27:
